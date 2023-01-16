4b77d109dbd3658c6ec465f5aeae69fb1dfea403
Mihai Dretcanu <mihai.dretcanu01@gmail.com>
2023-01-16 23:50:37 +0200

    Added Not Null validation for tagsIds in FilterRequest (seems to never be null though)

--- src/main/java/com/ebn/calendar/model/dto/request/FilterEventsRequest.java
+++ src/main/java/com/ebn/calendar/model/dto/request/FilterEventsRequest.java
@@ -1,8 +1,11 @@
 package com.ebn.calendar.model.dto.request;
 
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 public class FilterEventsRequest {
+
+    @NotNull(message = "'tagsIds' is mandatory")
     private Set<String> tagsIds;
 
     public Set<String> getTagsIds() {
